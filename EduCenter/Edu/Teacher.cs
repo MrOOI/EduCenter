b8e0928657ca2ef3604069444296c8f0fa94d0e4
Cracker <83668948+Cracker@users.noreply.github.com>
2021-11-02 00:54:19 +0300

    Add teacher class to the project

---
+++ EduCenter/Edu/Teacher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edu
+{
+    class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public string Level { get; set; }
+        private int Money { get; set; }
+
+        public Teacher()
+        {
+
+        }
+
+        public Teacher(string firstname, string lastname, string subject, string level)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Level = level;
+            this.Subject = subject;
+        }
+    }
+}
